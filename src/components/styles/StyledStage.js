80bd73edb3c948f900d0ff780ed45e41c57163f4
Cristian Montes <cmontesmujica@gmail.com>
2021-09-24 21:46:53 -0700

    working on stage bug

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -1,13 +1,13 @@
 import styled  from "styled-components";
 
 export const StyledStage =  styled.div`
-    display:grid;
-    grid-template-rows:repeat(
+    display: grid;
+    grid-template-rows: repeat(
         ${props => props.height},
-        calc(25vw/${props => props.width})
+        calc(25vw /${props => props.width})
     )
 
-    grid-template-colums: repeat(${props => props.width}, 1fr);
+    grid-template-columns: repeat(${props => props.width}, 1fr);
     grid-gap: 1px;
     border: 2px solid #333;
     width: 100%;
